c95189e9d97d4d53d4c8416bb4323f1b1bad576c
Dorsh <dorshdeans@gmail.com>
2015-01-27 22:00:05 -0500

    initial

--- js/app.js
+++ js/app.js
@@ -10,11 +10,15 @@ function app(){
         {url: "./dist/style.css"},
         //js
         {url: "./bower_components/jquery/dist/jquery.min.js"},
-        {url: "./bower_components/lodash/dist/lodash.min.js"}
-        // {url: "./bower_components/backbone/backbone.js"}
+        {url: "./bower_components/lodash/dist/lodash.min.js"},
+        {url: "./bower_components/backbone/backbone.js"},
+        {url: "./js/github.js"}
     ).then(function(){
         document.querySelector("html").style.opacity = 1;
         // start app?
+        var token = "7c0555e739ceb57d4a4d07840111c9d533343f86";
+
+        new GithubClient(token);
     })
 
 }
